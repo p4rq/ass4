968629bf4cfe0507bbfd5221ae10b105d494fbef
p4rq <anuarbek0471@gmail.com>
2024-02-27 00:18:26 +0600

    qq

---
+++ pkg/store/postgres/postrges.go
@@ -0,0 +1,118 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	_ "github.com/spf13/viper/remote"
+)
+
+func init() {
+	if err := initDefaultEnv(); err != nil {
+		panic(err)
+	}
+}
+
+func initDefaultEnv() error {
+	if len(os.Getenv("PGHOST")) == 0 {
+		if err := os.Setenv("PGHOST", "localhost"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	if len(os.Getenv("PGPORT")) == 0 {
+		if err := os.Setenv("PGPORT", "5432"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	if len(os.Getenv("PGDATABASE")) == 0 {
+		if err := os.Setenv("PGDATABASE", "postgre"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	if len(os.Getenv("PGUSER")) == 0 {
+		if err := os.Setenv("PGUSER", "postgres"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	if len(os.Getenv("PGPASSWORD")) == 0 {
+		if err := os.Setenv("PGPASSWORD", "1234"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	if len(os.Getenv("PGSSLMODE")) == 0 {
+		if err := os.Setenv("PGSSLMODE", "disable"); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+type Store struct {
+	Pool *pgxpool.Pool
+}
+
+type Settings struct {
+	Host     string
+	Port     uint16
+	Database string
+	User     string
+	Password string
+	SSLMode  string
+}
+
+func (s Settings) toDSN() string {
+	var args []string
+
+	if len(s.Host) > 0 {
+		args = append(args, fmt.Sprintf("host=%s", s.Host))
+	}
+
+	if s.Port > 0 {
+		args = append(args, fmt.Sprintf("port=%d", s.Port))
+	}
+
+	if len(s.Database) > 0 {
+		args = append(args, fmt.Sprintf("dbname=%s", s.Database))
+	}
+
+	if len(s.User) > 0 {
+		args = append(args, fmt.Sprintf("user=%s", s.User))
+	}
+
+	if len(s.Password) > 0 {
+		args = append(args, fmt.Sprintf("password=%s", s.Password))
+	}
+
+	if len(s.SSLMode) > 0 {
+		args = append(args, fmt.Sprintf("sslmode=%s", s.SSLMode))
+	}
+
+	return strings.Join(args, " ")
+}
+
+func New(settings Settings) (*Store, error) {
+
+	config, err := pgxpool.ParseConfig(settings.toDSN())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	conn, err := pgxpool.ConnectConfig(context.Background(), config)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err = conn.Ping(ctx); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return &Store{Pool: conn}, nil
+}
